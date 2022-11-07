1c4f53be7d1844e1821b050df01f92ff7b9b1da2
ArunrajMK <arunrajshanker6@gmail.com>
2022-11-08 00:42:27 +0530

    final

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,30 +1,33 @@
-import React, { useEffect, useState } from 'react'
+import React  from 'react'
 import "./all.css"
-function Cart({gt,addedArr,total}) {
+function Cart({ addedArr }) {
 
-console.log("fff",addedArr);
-  
- let out=addedArr?.reduce((a,b)=>{
-  
-  return Number(a)+Number(b.price*b.Q)
- },0)
- console.log("a",out);
+  console.log("fff", addedArr);
+
+  let out = addedArr?.reduce((a, b) => {
+
+    return Number(a) + Number(b.price * b.Q)
+  }, 0)
+
+
+
+
+
+  console.log("a", out);
 
   return (
     <div id="cart">
-      
-        {/* <div id="container">
-       
-        <div>Product Name: {name}</div> */}
-        <div> <h4>Cart</h4></div>
-         {addedArr.map((item)=>(
-          <>
+
+    
+      <div> <h4>Cart</h4></div>
+      {addedArr.map((item) => (
+        <>
           <h4>Name:{item.name}  Quantity: {item.Q}</h4>
-          
-          </>
-         ))}
-        <h3>Total:{out}</h3>
-       
+
+        </>
+      ))}
+      <h3>Total:{out}</h3>
+
     </div>
   )
 }
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,105 +1,96 @@
-import React, { useEffect, useState } from 'react'
+import React, {  useState } from 'react'
 import "./all.css"
 import Cart from './Cart'
-function Products({data}) {
-
-    const [total, setTotal] = useState()
-    const[addedArr,setAddedArr]=useState([])
-    const[quantity,setQuantity]=useState(1)
-    const [gt, setGt] = useState(0)
-
-    
-const handleAdd=(el)=>{
-    
-   let newArr=[...addedArr]
-    setTotal(total)
-    setGt(gt+el.price)
-
-    let arr1=add(newArr,el)
-    setAddedArr(arr1)
-}
-
-const add=(newArr,el)=>{
-    let f=true
-    console.log(newArr);
-    
-    newArr?.forEach((e)=>{
-        if(e.id==el.id){
-         e.Q+=1
-         f=false
-         
-        }
-     })
-     if(f){
-        newArr.push({...el,Q:1})
-     }
-    
-     return newArr
-}
-
-// let grndTotal=arrr.reduce((a,b)=>{
-//     return a+b},0)
-
-    const handleminus=(el)=>{
-        // if(el.count===1){
-        //     addedArr.pop()
-        // }
-        // el.quant--
-        // setTotal(total-1)
-        // setGt(gt-el.price)
-        // addedArr.pop()
-        // setAddedArr([...addedArr,el])
-    let newArr=[...addedArr]
-    // setTotal(total)
-    // setGt(gt+el.price)
-
-    let arr1=minus(newArr,el)
-    setAddedArr(arr1)
-    }
-
-
-    const minus=(newArr,el)=>{
-        let f=true
-        console.log(newArr);
-        
-        newArr?.forEach((e)=>{
-            if(e.id===el.id){
-             e.Q-=1
-             f=false
-             
-            }
-         })
-         if(f){
-            newArr.push({...el,Q:1})
-         }
-        
-         return newArr
-    }
-
-
-
-
-
-  return (
-    <>
-    {data?.map((el,index)=>{
-        return(
-            <div key={index} id="inBox">
-    <h1>{el.name}</h1>
-    <h2>{el.price}</h2>
-    <button onClick={()=>handleAdd(el)}>+</button>
-    <button onClick={()=>handleminus(el)}>-</button>
-   </div>
-        )
-    })}
-    
+function Products({ data }) {
 
   
+    const [addedArr, setAddedArr] = useState([])
+   
 
-    <Cart gt={gt} addedArr={addedArr} total={total}  />
 
-</>
-  )
+
+    const handleAdd = (el) => {
+
+        let newArr = [...addedArr]
+      
+       
+
+        let arr1 = add(newArr, el)
+        setAddedArr(arr1)
+    }
+
+    const add = (newArr, el) => {
+        let f = true
+        console.log(newArr);
+
+        newArr?.forEach((e) => {
+            if (e.id === el.id) {
+                e.Q += 1
+                f = false
+
+            }
+        })
+        if (f) {
+            newArr.push({ ...el, Q: 1 })
+        }
+
+        return newArr
+    }
+
+    
+
+    const handleminus = (el) => {
+      
+        let newArr = [...addedArr]
+       
+
+        let arr1 = minus(newArr, el)
+        setAddedArr(arr1)
+    }
+
+
+    const minus = (newArr, el) => {
+        let f = true
+        console.log(newArr);
+
+        newArr?.forEach((e) => {
+            if (e.id === el.id) {
+                e.Q -= 1
+                f = false
+                if (e.Q < 0) {
+                    e.Q = 0
+                }
+            }
+        })
+
+
+        return newArr
+    }
+
+
+
+
+
+    return (
+        <>
+            {data?.map((el, index) => {
+                return (
+                    <div key={index} id="inBox">
+                        <h1>{el.name}</h1>
+                        <h2>{el.price}</h2>
+                        <button onClick={() => handleAdd(el)}>+</button>
+                        <button onClick={() => handleminus(el)}>-</button>
+                    </div>
+                )
+            })}
+
+
+
+
+            <Cart  addedArr={addedArr}  />
+
+        </>
+    )
 }
 
 export default Products
